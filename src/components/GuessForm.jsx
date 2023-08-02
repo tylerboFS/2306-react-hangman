c415a3eb920167afd7c4838e71fba7a8ec88c194
Tyler Wright <tyler.wright@fullstackacademy.com>
2023-08-02 13:32:39 -0700

    Guess Form

--- src/components/GuessForm.jsx
+++ src/components/GuessForm.jsx
@@ -1,4 +1,32 @@
+import { useState } from "react";
+
 const GuessForm = () => {
-  return <h3>GuessForm</h3>
+  const [guessedLetter, setGuessedLetter] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    alert(guessedLetter);
+
+  };
+
+  return (
+    <>
+      <h3>Guess a Letter</h3>
+      <form onSubmit={submitHandler}>
+        <input
+          id="letterInput"
+          type="text"
+          value={guessedLetter}
+          onChange={(e) => setGuessedLetter(e.target.value.toUpperCase())}
+          pattern="[A-Za-z]{1}"
+          maxLength={1}
+          size={2}
+        />
+        
+        <button>Guess!</button>
+      </form>
+    </>
+  );
 };
 export default GuessForm;
