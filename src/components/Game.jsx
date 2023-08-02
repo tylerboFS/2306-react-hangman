b66ee81bb29ed0c5442adb1741786b4a8306db5c
Tyler Wright <tyler.wright@fullstackacademy.com>
2023-08-02 12:47:12 -0700

    Fetching word for word API

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,13 +1,34 @@
+import { useState, useEffect } from "react";
 import Graveyard from "./Graveyard";
 import GuessForm from "./GuessForm";
 import WordDisplay from "./WordDisplay";
 
 const Game = () => {
+  const [currentWord, setCurrentWord] = useState(null);
+  //const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // fetch a random word
+    const fetchWord = async () => {
+      try {
+        const response = await fetch(
+          "https://random-word-api.vercel.app/api?words=1"
+        );
+        const body = await response.json();
+        console.log(body[0]);
+        setCurrentWord(body[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchWord();
+  }, []);
+
   return (
     <>
       <h3>Game</h3>
       <GuessForm />
-      <WordDisplay />
+      <WordDisplay currentWord={currentWord} />
       <Graveyard />
     </>
   );
--- src/components/WordDisplay.jsx
+++ src/components/WordDisplay.jsx
@@ -1,4 +1,11 @@
-const WordDisplay = () => {
-  return <h3>WordDisplay</h3>
+const WordDisplay = (props) => {
+
+  // console.log("Current Word:", props.currentWord);
+  return (
+    <>
+      <h3>WordDisplay</h3>
+      <p>{props.currentWord ? props.currentWord : "Loading..."}</p>
+    </>
+  );
 };
 export default WordDisplay;
